9ec10ecc9c6a5de00a4b628c0cf656acccce197c
Sam Shahriary <sshahriary@ucdavis.edu>
2018-11-04 15:58:43 -0800

    get port from environment variable and normalize port number

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-//npm modules
+/* npm modules */
 var http = require('http');
-//node modules
+/* node modules */
 var app = require('./app.js');
 
 
-//global var
-const port = 3000;
-
-//store port in express
+/* get port from environment variable */
+var port = normalizeport(process.env.PORT || '3000');
+/* store port in express */
 app.set('port', port);
 
-//Listen on provided port, on all network interfaces.
+/* Listen on provided port, on all network interfaces. */
 var server = http.createServer(app);
 server.listen(port);
 
 module.exports = server;
+
+
+/*
+** parses string value into integer for port number usage
+** @param {string} value - requested port number
+*/
+function normalizeport(value){
+  var port = parseInt(value, 10);
+
+  if(isNaN(port)){
+    //named pipe
+    return value;
+  }
+
+  if(port >= 0){
+    //port number
+    return port;
+  }
+
+  return false;
+}
