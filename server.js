367e562d846ca1925fc63e7683ad6032fbbd9cba
Sam Shahriary <sshahriary@ucdavis.edu>
2018-11-04 16:09:25 -0800

    add error handling for server error events: in-use error, unauthorized access error

--- server.js
+++ server.js
@@ -12,9 +12,36 @@ app.set('port', port);
 /* Listen on provided port, on all network interfaces. */
 var server = http.createServer(app);
 server.listen(port);
+server.on('error', onError);
 
 module.exports = server;
 
+/*
+** event listener for http server "error" event
+** @param {object} error - specific error event
+*/
+function onError(error){
+
+  if(error.syscall !== 'listen'){
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  /* handle specific listen error with user friendly message */
+  switch(error.code){
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
 /*
 ** parses string value into integer for port number usage
